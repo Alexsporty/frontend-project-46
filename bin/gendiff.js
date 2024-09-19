007b728d6a4b2945bac3e9309b24465f0d7d2d06
Aleksandr Kuptsov <kuptsov.sasha@mail.ru>
2024-09-19 12:08:26 +0300

    my second project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-
+import fs from 'fs';
+import path from 'path';
 
 program
   .name('gendiff')
@@ -8,5 +9,14 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('')
   .option('-f, --format <type>', 'output format')
-  
-program.parse();
+  .action((filepath1, filepath2) => {
+    const file1 = fs.readFileSync(path.resolve(filepath1), 'utf8');
+    const file2 = fs.readFileSync(path.resolve(filepath2), 'utf8');
+    const json1 = JSON.parse(file1);
+    const json2 = JSON.parse(file2);
+    const diff = JSON.stringify(json1) !== JSON.stringify(json2)
+      ? 'Файлы отличаются'
+      : 'Файлы идентичны';
+      console.log(diff);
+  });
+program.parse(process.argv);
