7d0bcf38c73ff9585f699e8b00c628b0d8503453
Aleksandr Kuptsov <kuptsov.sasha@mail.ru>
2024-10-29 10:48:13 +0300

    my second project

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,18 @@
 import yaml from 'js-yaml';
 
 const parser = (filepath, ext) => {
-  if (ext === '.json') {
-    return JSON.parse(filepath);
-  } if (ext === '.yml' || ext === '.yaml') {
-    return yaml.load(filepath);
+  switch (ext) {
+    case '.json':
+      return JSON.parse(filepath);
+
+    case '.yml':
+      return yaml.load(filepath);
+
+    case '.yaml':
+      return yaml.load(filepath);
+
+    default:
+      throw new Error(`Unsupported format: ${filepath}`);
   }
-  throw new Error(`Unsupported format: ${filepath}`);
 };
 export default parser;
